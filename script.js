3549ca5ce724fdfbd16b3a33919dfdc731ca47d7
Ken <knetngyn@gmail.com>
2022-07-27 21:21:29 -0500

    Add delete button function

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ let author = document.querySelector("#author");
 let pages = document.querySelector("#pages");
 let readStatus = document.querySelector("#read");
 let btn = document.querySelector(".submit");
+let shelf = document.querySelector(".shelfMain");
+
+document.addEventListener("click", deleteListener);
+
+function deleteListener(e) {
+  let element = e.target;
+  if (element.classList.contains("deleteBtn")) {
+    myLibrary.splice(e.composedPath()[1].dataset.num, 1);
+    shelf.innerHTML = "";
+    addToShelf();
+  }
+}
 
 btn.addEventListener("click", () => {
   if (book.value == "" || author.value == "" || pages.value == "") {
     alert("empty");
   } else {
+    shelf.innerHTML = "";
     addBookToLibrary(
       bookTitle.value,
       author.value,
       pages.value,
       readStatus.value
     );
+    addToShelf();
   }
 });
 
@@ -29,4 +43,37 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(bookTitle, author, pages, readStatus) {
   let newBook = new Book(bookTitle, author, pages, readStatus);
   myLibrary.push(newBook);
+}
+
+function addToShelf() {
+  for (let book of myLibrary) {
+    let titleEl = document.createElement("p");
+    let authorEl = document.createElement("p");
+    let pageEl = document.createElement("p");
+    let readEl = document.createElement("button");
+    let deleteEl = document.createElement("button");
+    deleteEl.classList.add("deleteBtn");
+    deleteEl.textContent += "DELETE";
+    for (let label in book) {
+      if (label == "title") {
+        titleEl.textContent += book[label];
+      } else if (label == "author") {
+        authorEl.textContent += book[label];
+      } else if (label == "pages") {
+        pageEl.textContent += book[label];
+      } else if (label == "read") {
+        readEl.textContent += book[label];
+      }
+      //   console.log(`${label}: ${book[label]}`);
+    }
+    let newBook = document.createElement("div");
+    newBook.dataset.num = myLibrary.indexOf(book);
+    newBook.classList.add("bookObj");
+    newBook.appendChild(titleEl);
+    newBook.appendChild(authorEl);
+    newBook.appendChild(pageEl);
+    newBook.appendChild(readEl);
+    newBook.appendChild(deleteEl);
+    shelf.appendChild(newBook);
+  }
 }
